87302c0e363a9a5eefc1e3d77522c1bfc7724524
ZeK1ng <dgogi17@freeuni.edu.ge>
2021-04-21 21:54:58 +0400

    Done

--- RSP/app/src/main/java/ge/dgoginashvili/rockpapersscissors/MainActivity.kt
+++ RSP/app/src/main/java/ge/dgoginashvili/rockpapersscissors/MainActivity.kt
@@ -19,68 +19,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun playGame() {
-        findViewById<Button>(R.id.rockButton).setOnClickListener {
-            findViewById<TextView>(R.id.computerScore).setTextColor(Color.BLACK)
-            findViewById<TextView>(R.id.playerScore).setTextColor(Color.BLACK)
-            Log.d("Rock Pressed", "Rock")
-            findViewById<TextView>(R.id.startText).visibility = View.GONE
-            val computerTurn = getComputerTurn()
-            Log.i("Computer Action=",computerTurn)
-            findViewById<ImageView>(R.id.userImage).setImageResource(R.drawable.rock_image)
-            findViewById<ImageView>(R.id.userImage).visibility = View.VISIBLE
-            if (computerTurn == "Rock") {
-                print("Show comp rock image")
-                findViewById<TextView>(R.id.computerScore).setTextColor(Color.YELLOW)
-                findViewById<TextView>(R.id.playerScore).setTextColor(Color.YELLOW)
-                findViewById<ImageView>(R.id.computerImage).setImageResource(R.drawable.rock_image)
-                findViewById<ImageView>(R.id.computerImage).visibility = View.VISIBLE
-            }
-            if (computerTurn == "Paper") {
-                computerScore++
-                findViewById<TextView>(R.id.computerScore).text = computerScore.toString()
-                findViewById<TextView>(R.id.computerScore).setTextColor(Color.GREEN)
-                findViewById<ImageView>(R.id.computerImage).setImageResource(R.drawable.paper_image)
-                findViewById<ImageView>(R.id.computerImage).visibility = View.VISIBLE
-            }
-            if (computerTurn == "Scissor") {
-                userScore++
-                findViewById<TextView>(R.id.playerScore).text = userScore.toString()
-                findViewById<TextView>(R.id.playerScore).setTextColor(Color.GREEN)
-                findViewById<ImageView>(R.id.computerImage).setImageResource(R.drawable.scissors_image)
-                findViewById<ImageView>(R.id.computerImage).visibility = View.VISIBLE
-            }
-        }
-        findViewById<Button>(R.id.paperButton).setOnClickListener {
-            findViewById<TextView>(R.id.computerScore).setTextColor(Color.BLACK)
-            findViewById<TextView>(R.id.playerScore).setTextColor(Color.BLACK)
-            Log.d("paperButton Pressed", "paperButton")
-            findViewById<TextView>(R.id.startText).visibility = View.GONE
-            findViewById<ImageView>(R.id.userImage).setImageResource(R.drawable.paper_image)
-            findViewById<ImageView>(R.id.userImage).visibility = View.VISIBLE
-            val computerTurn = getComputerTurn()
-            Log.i("Computer Action=",computerTurn)
+        rockButtonListener()
+        paperButtonListener()
+        szrButtonListener()
+    }
 
-            if (computerTurn == "Rock") {
-                userScore++
-                findViewById<TextView>(R.id.playerScore).text = userScore.toString()
-                findViewById<TextView>(R.id.playerScore).setTextColor(Color.GREEN)
-                findViewById<ImageView>(R.id.computerImage).setImageResource(R.drawable.rock_image)
-                findViewById<ImageView>(R.id.computerImage).visibility = View.VISIBLE
-            }
-            if (computerTurn == "Paper") {
-                findViewById<TextView>(R.id.computerScore).setTextColor(Color.YELLOW)
-                findViewById<TextView>(R.id.playerScore).setTextColor(Color.YELLOW)
-                findViewById<ImageView>(R.id.computerImage).setImageResource(R.drawable.paper_image)
-                findViewById<ImageView>(R.id.computerImage).visibility = View.VISIBLE
-            }
-            if (computerTurn == "Scissor") {
-                computerScore++
-                findViewById<TextView>(R.id.computerScore).text = computerScore.toString()
-                findViewById<TextView>(R.id.computerScore).setTextColor(Color.GREEN)
-                findViewById<ImageView>(R.id.computerImage).setImageResource(R.drawable.scissors_image)
-                findViewById<ImageView>(R.id.computerImage).visibility = View.VISIBLE
-            }
-        }
+    private fun szrButtonListener() {
         findViewById<Button>(R.id.szrButton).setOnClickListener {
             findViewById<TextView>(R.id.computerScore).setTextColor(Color.BLACK)
             findViewById<TextView>(R.id.playerScore).setTextColor(Color.BLACK)
@@ -112,12 +56,77 @@ class MainActivity : AppCompatActivity() {
                 findViewById<ImageView>(R.id.computerImage).visibility = View.VISIBLE
             }
         }
+    }
 
+    private fun paperButtonListener() {
+        findViewById<Button>(R.id.paperButton).setOnClickListener {
+            findViewById<TextView>(R.id.computerScore).setTextColor(Color.BLACK)
+            findViewById<TextView>(R.id.playerScore).setTextColor(Color.BLACK)
+            Log.d("paperButton Pressed", "paperButton")
+            findViewById<TextView>(R.id.startText).visibility = View.GONE
+            findViewById<ImageView>(R.id.userImage).setImageResource(R.drawable.paper_image)
+            findViewById<ImageView>(R.id.userImage).visibility = View.VISIBLE
+            val computerTurn = getComputerTurn()
+            Log.i("Computer Action=",computerTurn)
+
+            if (computerTurn == "Rock") {
+                userScore++
+                findViewById<TextView>(R.id.playerScore).text = userScore.toString()
+                findViewById<TextView>(R.id.playerScore).setTextColor(Color.GREEN)
+                findViewById<ImageView>(R.id.computerImage).setImageResource(R.drawable.rock_image)
+                findViewById<ImageView>(R.id.computerImage).visibility = View.VISIBLE
+            }
+            if (computerTurn == "Paper") {
+                findViewById<TextView>(R.id.computerScore).setTextColor(Color.YELLOW)
+                findViewById<TextView>(R.id.playerScore).setTextColor(Color.YELLOW)
+                findViewById<ImageView>(R.id.computerImage).setImageResource(R.drawable.paper_image)
+                findViewById<ImageView>(R.id.computerImage).visibility = View.VISIBLE
+            }
+            if (computerTurn == "Scissor") {
+                computerScore++
+                findViewById<TextView>(R.id.computerScore).text = computerScore.toString()
+                findViewById<TextView>(R.id.computerScore).setTextColor(Color.GREEN)
+                findViewById<ImageView>(R.id.computerImage).setImageResource(R.drawable.scissors_image)
+                findViewById<ImageView>(R.id.computerImage).visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun rockButtonListener() {
+        findViewById<Button>(R.id.rockButton).setOnClickListener {
+            findViewById<TextView>(R.id.computerScore).setTextColor(Color.BLACK)
+            findViewById<TextView>(R.id.playerScore).setTextColor(Color.BLACK)
+            Log.d("Rock Pressed", "Rock")
+            findViewById<TextView>(R.id.startText).visibility = View.GONE
+            val computerTurn = getComputerTurn()
+            Log.i("Computer Action=",computerTurn)
+            findViewById<ImageView>(R.id.userImage).setImageResource(R.drawable.rock_image)
+            findViewById<ImageView>(R.id.userImage).visibility = View.VISIBLE
+            if (computerTurn == "Rock") {
+                findViewById<TextView>(R.id.computerScore).setTextColor(Color.YELLOW)
+                findViewById<TextView>(R.id.playerScore).setTextColor(Color.YELLOW)
+                findViewById<ImageView>(R.id.computerImage).setImageResource(R.drawable.rock_image)
+                findViewById<ImageView>(R.id.computerImage).visibility = View.VISIBLE
+            }
+            if (computerTurn == "Paper") {
+                computerScore++
+                findViewById<TextView>(R.id.computerScore).text = computerScore.toString()
+                findViewById<TextView>(R.id.computerScore).setTextColor(Color.GREEN)
+                findViewById<ImageView>(R.id.computerImage).setImageResource(R.drawable.paper_image)
+                findViewById<ImageView>(R.id.computerImage).visibility = View.VISIBLE
+            }
+            if (computerTurn == "Scissor") {
+                userScore++
+                findViewById<TextView>(R.id.playerScore).text = userScore.toString()
+                findViewById<TextView>(R.id.playerScore).setTextColor(Color.GREEN)
+                findViewById<ImageView>(R.id.computerImage).setImageResource(R.drawable.scissors_image)
+                findViewById<ImageView>(R.id.computerImage).visibility = View.VISIBLE
+            }
+        }
     }
 
     private fun getComputerTurn(): String {
-        val randomInteger = (1..3).shuffled().first()
-        when (randomInteger) {
+        when (val randomInteger = (1..3).shuffled().first()) {
             1 -> return "Rock"
             2 -> return "Paper"
             3 -> return "Scissor"
