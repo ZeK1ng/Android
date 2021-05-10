14c60285638840633dc122cd3b88df1ea3cbf46b
ZeK1ng <dgogi17@freeuni.edu.ge>
2021-05-10 20:59:25 +0400

    in prog

--- WeatherApp/app/src/main/java/ge/dgoginashvili/weatherapp/ApiService.kt
+++ WeatherApp/app/src/main/java/ge/dgoginashvili/weatherapp/ApiService.kt
@@ -6,4 +6,7 @@ import retrofit2.http.*
 interface ApiService {
     @GET("/data/2.5/weather")
     fun getWeather(@QueryMap params:Map<String, String>): Call<WeatherInfoModel>
+
+    @GET("/data/2.5/forecast")
+    fun get5DayWeather(@QueryMap params:Map<String, String>): Call<List<WeatherInfoModel>>
 }
